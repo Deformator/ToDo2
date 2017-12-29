bd3c06682dd7f7615b982c0d3159cdab4966c4a4
Deformator <dammandrii@gmail.com>
2017-12-29 17:29:51 -0500

    Create View Controllers and implement base design

---
+++ ToDo2/TaskViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TaskViewController.swift
+//  ToDo2
+//
+//  Created by Andrii Damm on 2017-12-29.
+//  Copyright © 2017 Andrii Damm. All rights reserved.
+//  Version: 0.2
+//  Commite: Create View Controllers and implement base design
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
